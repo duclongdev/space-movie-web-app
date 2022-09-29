87016324b08f3048e7fdfcf36908218efef7ebe3
duclongdev <20521566@gm.uit.edu.vn>
2022-09-29 23:09:44 +0700

    add dark mode

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import useDarkMode from '../../hooks/useDarkMode';
 
 const Header = () => {
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
+  const test =()=>{
+    toggleDarkMode(!isDarkMode);
+  }
   return (
-    <div>Header</div>
-  )
+    <div>
+
+      <button type="button" onClick={test} >Click Me!</button>
+    </div>
+  );
 }
 
 export default Header
