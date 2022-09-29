0f476f01bd478303ebd40fc393894a54b54ebd9e
duclongdev <20521566@gm.uit.edu.vn>
2022-09-29 16:55:39 +0700

    congif tailwind

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default Home
