79742c657140ae81adb7e1870de522be1ffa15b8
Đỗ Phạm Huy Khánh <20521442@gm.uit.edu.vn>
2023-02-03 19:40:45 +0700

    adding trailer section

--- src/pages/play/index.jsx
+++ src/pages/play/index.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/navigation";
 
-import { Navigation } from "swiper";
 import My_Swiper from "./swiper";
 import My_Recommend from "./recommend_swiper"
 
